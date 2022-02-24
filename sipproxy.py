9d5fa9575caff8f2a131b6ad24a1c8b9e6e62a36
plugin23 <xvalicek@stuba.sk>
2022-02-24 15:36:47 +0100

    using sipfullproxy as library

--- sipproxy.py
+++ sipproxy.py
@@ -1,4 +1,18 @@
 import sipfullproxy
+import logging
+import socketserver
+import socket
 
 def main():
-    sipfullproxy.ipaddress
+    logger = sipfullproxy.initialize_logger("sipproxy")
+    hostname = socket.gethostname()
+    logger.info(hostname)
+    ipaddress = socket.gethostbyname(hostname)
+    logger.info(ipaddress)
+
+    sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress, sipfullproxy.PORT)
+    sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress, sipfullproxy.PORT)
+    server = socketserver.UDPServer((sipfullproxy.HOST, sipfullproxy.PORT), sipfullproxy.UDPHandler)
+    server.serve_forever()
+
+main()
